98812115ba85de4c03beb24014917d92e96c9daf
gxl <hzguxiaolong@corp.netease.com>
2023-11-13 20:59:48 +0800

    fix(dockerfile): 端口修正

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # build stage
-FROM node:lts-alpine AS build-stage
+FROM node:latest AS build-stage
 # Set environment variables for non-interactive npm installs
 ENV NPM_CONFIG_LOGLEVEL warn
 ENV CI true
@@ -13,5 +13,5 @@ RUN pnpm build
 FROM nginx:stable-alpine AS production-stage
 COPY --from=build-stage /app/dist /usr/share/nginx/html
 COPY nginx.conf /etc/nginx/conf.d/default.conf
-EXPOSE 80:
+EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -o errexit
+echo "###############开始构建Docker镜像###############"
+v="latest"
+l="y"
+while getopts ":v:l:" opt;
+do
+    case $opt in
+        v) v="$OPTARG";;
+        l) l="$OPTARG";;
+    esac
+done
+echo "镜像版本：$v"
+echo "仅本地构建：$l"
+
+get_arch=`arch`
+p="linux/amd64"
+arm64="arm64"
+if [ "$get_arch" == "$arm64" ]; then
+    p="linux/arm64"
+fi
+echo "平台架构：$p"
+
+is_local="y"
+if [ "$l" == "$is_local" ]; then
+    echo "开始构建本地镜像，忽略参数v=$v"
+    docker buildx build -t fooololo/it-tools:latest --platform $p --load .
+else
+    echo "开始构建并推送镜像,版本：$v"
+    docker buildx build -t fooololo/it-tools:$v --platform linux/amd64 --push .
+
+    echo "开始构建并推送镜像,版本：latest"
+    docker buildx build -t fooololo/it-tools:latest --platform linux/amd64 --push .
+fi
+
+echo "构建镜像完成"
