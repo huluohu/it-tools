3fc1ec2935b794a20acf9599408b39d07905faf9
gxl <hzguxiaolong@corp.netease.com>
2023-11-20 12:03:35 +0800

    feat(ulid-generator): 完成ulid-generaor翻译

--- src/tools/ulid-generator/ulid-generator.vue
+++ src/tools/ulid-generator/ulid-generator.vue
@@ -3,6 +3,7 @@ import { ulid } from 'ulid';
 import _ from 'lodash';
 import { computedRefreshable } from '@/composable/computedRefreshable';
 import { useCopy } from '@/composable/copy';
+const { t } = useI18n();
 
 const amount = useStorage('ulid-generator-amount', 1);
 const formats = [{ label: 'Raw', value: 'raw' }, { label: 'JSON', value: 'json' }] as const;
@@ -24,11 +25,11 @@ const { copy } = useCopy({ source: ulids, text: 'ULIDs copied to the clipboard' 
 <template>
   <div flex flex-col justify-center gap-2>
     <div flex items-center>
-      <label w-75px> Quantity:</label>
+      <label w-75px> {{ t('tools.ulid-generator.function.quantity')  }}</label>
       <n-input-number v-model:value="amount" min="1" max="100" flex-1 />
     </div>
 
-    <c-buttons-select v-model:value="format" :options="formats" label="Format: " label-width="75px" />
+    <c-buttons-select v-model:value="format" :options="formats" :label="t('tools.ulid-generator.function.format')" label-width="75px" />
 
     <c-card mt-5 flex data-test-id="ulids">
       <pre m-0 m-x-auto>{{ ulids }}</pre>
@@ -36,10 +37,10 @@ const { copy } = useCopy({ source: ulids, text: 'ULIDs copied to the clipboard' 
 
     <div flex justify-center gap-2>
       <c-button data-test-id="refresh" @click="refreshUlids()">
-        Refresh
+        {{t('tools.ulid-generator.function.refresh')}}
       </c-button>
       <c-button @click="copy()">
-        Copy
+        {{t('tools.ulid-generator.function.copy')}}
       </c-button>
     </div>
   </div>
